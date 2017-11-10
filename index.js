6255d4804ea178c8017d1dca17bd19ce940ba877
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-10 03:57:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.random() * (20-0);
 
-randomInteger = Math.floor(randomNumber);
+let randomInteger = Math.floor(randomNumber);
