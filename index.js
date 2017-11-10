65593d0576ad5c18e71d28130f51462d376566ed
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-10 03:36:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,3 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 const newID = 1000000000 + oldID ;
 
-function ageIsValid {
-if (currentAgeIsInteger) {
-  expect(ageIsValid).to.be.true;
-} else {
-  expect(ageIsValid).to.be.false;
-}
-}
