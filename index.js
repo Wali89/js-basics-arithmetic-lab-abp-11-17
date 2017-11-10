59d3d6e4ba8918f94222a550b5166ca99ea2ae56
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-10 04:11:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ let randomNumber = Math.random() * (20-0);
 
 let randomInteger = Math.floor(randomNumber);
 
-let randomUserID = randomInteger + 1000000001;
+let randomUserID = 'randomInteger' + 1000000001;
